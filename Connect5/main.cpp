4ecef1ccc87c7cf9ffc14f3d7369cbebcf00074c
Vikash Anand <vikashanand.sasi@gmail.com>
2021-04-09 09:04:36 +0530

    Initial Commit

---
+++ Connect5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Connect5
+//
+//  Created by Vikash Anand on 09/04/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
